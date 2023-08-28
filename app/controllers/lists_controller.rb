3fc3321361a991eb300b48ecdb0ccde289bcb5c2
Yukari Hangai <129511468+Yukari-Hangai@users.noreply.github.com>
2023-08-28 05:40:32 +0000

    Rubocop

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,12 +1,12 @@
 class ListsController < ApplicationController
   def new
-    @list = List.new#Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
+    @list = List.new # Viewへ渡すためのインスタンス変数に空のModelオブジェクトを生成する。
   end
 
   def create
     @list = List.new(list_params)
     if @list.save
-     redirect_to list_path(@list.id)
+      redirect_to list_path(@list.id)
     else
       render :new
     end
@@ -31,13 +31,13 @@ class ListsController < ApplicationController
   end
 
   def destroy
-    list = List.find(params[:id]) #データ（レコード）を1件取得
-    list.destroy#データ（レコード）を削除
-    redirect_to '/lists'#投稿一覧画面へリダイレクト
+    list = List.find(params[:id]) # データ（レコード）を1件取得
+    list.destroy # データ（レコード）を削除
+    redirect_to "/lists" # 投稿一覧画面へリダイレクト
   end
 
-  private#ストロングパラメータ
-  def list_params
-    params.require(:list).permit(:title,:body,:image)
-  end
+  private # ストロングパラメータ
+    def list_params
+      params.require(:list).permit(:title, :body, :image)
+    end
 end
